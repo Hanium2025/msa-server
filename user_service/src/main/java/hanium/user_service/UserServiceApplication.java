6f3d79feb9848bf7ce1682a1c578bfc69a3416bb
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-06-26 19:38:10 +0900

    [feat/#2]: .gitignore 설정

---
+++ user_service/src/main/java/hanium/user_service/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package hanium.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableDiscoveryClient
+@EnableAsync
+@ConfigurationPropertiesScan  // 설정 클래스 스캔을 활성화
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
