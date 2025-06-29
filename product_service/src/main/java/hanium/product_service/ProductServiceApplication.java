7e53ae0da8c13d446f92db47f489ae5babc35b70
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-06-29 14:55:55 +0900

    [feat/#2]: product_service - 도커파일 추가 및 빌드설정 완료

---
+++ product_service/src/main/java/hanium/product_service/ProductServiceApplication.java
@@ -0,0 +1,19 @@
+package hanium.product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableDiscoveryClient
+@EnableAsync
+@ConfigurationPropertiesScan  // 설정 클래스 스캔을 활성화
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
