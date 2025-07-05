76abd7053f77bb42838bba38fffb8bcc36d87130
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-07-05 12:30:04 +0900

    [infra/#5]: All service - 동적 포트 반영을 위한 EurekaPortRegister 적용

--- notification_service/src/main/java/hanium/notification_service/EurekaPortRegister.java
+++ notification_service/src/main/java/hanium/notification_service/EurekaPortRegister.java
@@ -1,2 +1,37 @@
-package hanium.notification_service;public class EurekaPortRegister {
+package hanium.notification_service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistration;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 동적으로 할당된 포트를 Eureka 서버에 반영하는 컴포넌트입니다.
+ *
+ * Spring Boot 애플리케이션이 시작된 이후 실제 할당된 HTTP 포트를
+ * Eureka 인스턴스 설정에 적용하여, 클라이언트가 정확한 포트로 접근할 수 있도록 합니다.
+ *
+ * 이 설정은 server.port=0 과 같이 포트를 자동 할당하는 경우에 필수입니다.
+ */
+@Component
+public class EurekaPortRegister {
+
+    @Autowired
+    private WebServerApplicationContext webServerAppCtxt;
+    @Qualifier("eurekaRegistration")
+    @Autowired
+    private EurekaRegistration eurekaRegistration;
+    @EventListener
+    public void onApplicationReady(ApplicationReadyEvent event) {
+        // 실제 할당된 HTTP 포트 조회
+        int actualPort = webServerAppCtxt.getWebServer().getPort();
+        System.out.println("실제 할당된 HTTP 포트: " + actualPort);
+
+        // Eureka 등록 정보에 실제 포트 반영
+        eurekaRegistration.getInstanceConfig().setNonSecurePort(actualPort);
+
+    }
 }
--- user_service/src/main/java/hanium/user_service/EurekaPortRegister.java
+++ user_service/src/main/java/hanium/user_service/EurekaPortRegister.java
@@ -1,5 +1,4 @@
 package hanium.user_service;
-import com.netflix.appinfo.EurekaInstanceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -8,7 +7,14 @@ import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistrati
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-
+/**
+ * 동적으로 할당된 포트를 Eureka 서버에 반영하는 컴포넌트입니다.
+ *
+ * Spring Boot 애플리케이션이 시작된 이후 실제 할당된 HTTP 포트를
+ * Eureka 인스턴스 설정에 적용하여, 클라이언트가 정확한 포트로 접근할 수 있도록 합니다.
+ *
+ * 이 설정은 server.port=0 과 같이 포트를 자동 할당하는 경우에 필수입니다.
+ */
 @Component
 public class EurekaPortRegister {
 
@@ -19,11 +25,10 @@ public class EurekaPortRegister {
     private EurekaRegistration eurekaRegistration;
     @EventListener
     public void onApplicationReady(ApplicationReadyEvent event) {
+        // 실제 할당된 HTTP 포트 조회
         int actualPort = webServerAppCtxt.getWebServer().getPort();
         System.out.println("실제 할당된 HTTP 포트: " + actualPort);
-        // TODO: Eureka Instance 정보에 이 포트 반영 (필요시)
-
-        // 실제 포트를 Eureka 인스턴스에 반영
+        // Eureka 등록 정보에 실제 포트 반영
         eurekaRegistration.getInstanceConfig().setNonSecurePort(actualPort);
 
     }
