a330132962259aded822792716837d0fbdbef3e1
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-07-05 12:15:51 +0900

    테스트 코드 주석 처리

--- product_service/src/test/java/hanium/product_service/ProductServiceApplicationTests.java
+++ product_service/src/test/java/hanium/product_service/ProductServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package hanium.product_service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProductServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package hanium.product_service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProductServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
