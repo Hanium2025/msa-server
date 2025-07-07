fc2d2469248c9d64a7bf43fa94135a06b6a2644e
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-07-07 14:42:45 +0900

    [infra/#9] community_service - PostRepository interface, PostRepositoryImpl 추가

---
+++ community_service/src/main/java/hanium/community_service/Repository/impl/PostRepositoryImpl.java
@@ -0,0 +1,9 @@
+package hanium.community_service.Repository.impl;
+/**
+ * PostRepositoryImpl 클래스는 게시글 관련 추가 커스텀 구현을 위해
+ * 추후에 작성될 수 있는 클래스입니다.
+ *
+ * 현재는 구현 내용이 없으며, 필요 시 JpaRepository 커스터마이징을 위해 사용됩니다.
+ */
+public class PostRepositoryImpl {
+}
