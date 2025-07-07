931eb4b91c41a6d42eed34d72b4de993af2160f2
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-07-07 14:46:08 +0900

    [infra/#9] apigateway_service - 컨트롤러 구현

--- apigateway_service/src/main/java/hanium/apigateway_service/controller/CommunityController.java
+++ apigateway_service/src/main/java/hanium/apigateway_service/controller/CommunityController.java
@@ -1,18 +1,36 @@
 package hanium.apigateway_service.controller;
 
+import hanium.apigateway_service.dto.CommonResponseDTO;
+import hanium.apigateway_service.dto.CreatePostRequestDTO;
 import hanium.apigateway_service.grpc.CommunityGrpcClient;
+import hanium.apigateway_service.response.ResponseDTO;
+import hanium.common.proto.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/community")
 public class CommunityController {
 
     private final CommunityGrpcClient grpcClient;
 
-    @GetMapping("/community/ping")
-    public String pingCommunityService() {
-        return grpcClient.ping();
+//    @GetMapping("/ping")
+//    public String pingCommunityService() {
+//        return grpcClient.ping();
+//    }
+
+    @PostMapping("/post")
+    public ResponseEntity<ResponseDTO<CommonResponseDTO>> createPost(@RequestBody CreatePostRequestDTO dto) {
+        CommonResponse protoResponse = grpcClient.createPost(dto);
+        CommonResponseDTO responseDTO = CommonResponseDTO.fromProto(protoResponse);
+
+        ResponseDTO<CommonResponseDTO> response = new ResponseDTO<>(
+                responseDTO,
+                responseDTO.isSuccess() ? HttpStatus.OK : HttpStatus.BAD_REQUEST
+        );
+        return ResponseEntity.status(response.getCode()).body(response);
     }
 }
