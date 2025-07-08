92f032844a188d6ee45f8eab92d396bd893b63b7
mminjukim <mminju.km@gmail.com>
2025-07-08 18:49:58 +0900

    [feat/#12] user_service .proto 수정, gRPC 메서드 작성

---
+++ user_service/src/main/java/hanium/user_service/grpc/UserServiceGrpcImpl.java
@@ -0,0 +1,55 @@
+package hanium.user_service.grpc;
+
+import hanium.common.proto.CommonResponse;
+import hanium.common.proto.user.SignUpRequest;
+import hanium.common.proto.user.UserServiceGrpc;
+import hanium.user_service.dto.request.MemberSignupRequestDto;
+import hanium.user_service.mapper.MemberMapper;
+import hanium.user_service.service.MemberService;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
+
+@GrpcService
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceGrpcImpl extends UserServiceGrpc.UserServiceImplBase {
+
+    @Value("${eureka.instance.hostname:unknown-host}")
+    private String hostname;
+
+    private final MemberService memberService;
+
+    @Override
+    public void signUp(SignUpRequest request, StreamObserver<CommonResponse> responseObserver) {
+        try {
+            // grpc -> dto 변환
+            MemberSignupRequestDto dto = MemberMapper.toSignupDto(request);
+            // 서비스 호출
+            memberService.signup(dto);
+            // 성공 응답 생성
+            CommonResponse response = CommonResponse.newBuilder()
+                    .setSuccess(true)
+                    .setMessage("회원가입에 성공했습니다 - " + hostname)
+                    .setErrorCode(0)
+                    .build();
+            // 응답 전송
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        } catch (Exception e) {
+            log.error("회원가입 실패", e);
+            CommonResponse response = CommonResponse.newBuilder()
+                    .setSuccess(false)
+                    .setMessage("회원가입에 실패했습니다 - " + hostname + "\nErrorMessage: " + e.getMessage())
+                    .setErrorCode(-1)
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+}
--- user_service/src/main/java/hanium/user_service/mapper/MemberMapper.java
+++ user_service/src/main/java/hanium/user_service/mapper/MemberMapper.java
@@ -1,6 +1,8 @@
 package hanium.user_service.mapper;
 
+import hanium.common.proto.user.SignUpRequest;
 import hanium.user_service.domain.Member;
+import hanium.user_service.dto.request.MemberSignupRequestDto;
 import hanium.user_service.dto.response.MemberResponseDto;
 
 public class MemberMapper {
@@ -11,6 +13,19 @@ public class MemberMapper {
                 .id(member.getId())
                 .email(member.getEmail())
                 .phoneNumber(member.getPhoneNumber())
+                .build();
+    }
+
+    // SignUpRequest gRPC -> dto
+    public static MemberSignupRequestDto toSignupDto(SignUpRequest request) {
+        return MemberSignupRequestDto.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .confirmPassword(request.getConfirmPassword())
+                .phoneNumber(request.getPhoneNumber())
+                .nickname(request.getNickname())
+                .agreeMarketing(request.getAgreeMarketing())
+                .agreeThirdParty(request.getAgreeThirdParty())
                 .build();
     }
 }
