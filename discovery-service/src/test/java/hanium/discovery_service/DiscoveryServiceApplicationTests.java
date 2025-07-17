010909fbf012f34bfde035d1b4520120fdeed933
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-07-17 20:35:36 +0900

    [infra/#23] : user, discovery test code - 안쓰는 테스트 코드 주석처리

--- discovery-service/src/test/java/hanium/discovery_service/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/hanium/discovery_service/DiscoveryServiceApplicationTests.java
@@ -1,13 +1,15 @@
-package hanium.discovery_service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DiscoveryServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package hanium.discovery_service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest(classes = DiscoveryServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@ActiveProfiles("test")
+//class DiscoveryServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
