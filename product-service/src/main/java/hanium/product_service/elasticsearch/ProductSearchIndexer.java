24b2ed69e262763706560f4d5b6ba20d0a4a695d
gahyeon <gimgahyeon12@gmail.com>
2025-08-24 19:06:16 +0900

    [feat/#35] : product - Elasticsearch 인덱싱 처리 로직 추가

---
+++ product-service/src/main/java/hanium/product_service/elasticsearch/ProductSearchIndexer.java
@@ -0,0 +1,28 @@
+package hanium.product_service.elasticsearch;
+
+import hanium.product_service.domain.Product;
+import hanium.product_service.dto.response.ProductResponseDTO;
+import hanium.product_service.repository.ProductSearchRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductSearchIndexer {
+
+    private final ProductSearchElasticRepository productSearchElasticRepository;
+
+
+    public void index(Product product) {
+
+        ProductDocument doc = ProductDocument.from(product);
+        productSearchElasticRepository.save(doc);
+    }
+
+    public void remove(Long productId) {
+        productSearchElasticRepository.deleteById(productId);
+    }
+}
+
