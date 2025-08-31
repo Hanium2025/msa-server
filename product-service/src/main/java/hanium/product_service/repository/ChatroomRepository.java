f44080fe9840e089f581e7ba33006c7fbd643344
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-08-31 13:29:11 +0900

    [chat/#68] : product - 채팅방 존재 유무 양방향 조건 @Query로 수정

--- product-service/src/main/java/hanium/product_service/repository/ChatroomRepository.java
+++ product-service/src/main/java/hanium/product_service/repository/ChatroomRepository.java
@@ -2,20 +2,22 @@ package hanium.product_service.repository;
 
 import hanium.product_service.domain.Chatroom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
 
-    
     //해당 채팅방 존재 여부 확인
-    Optional<Chatroom> findByProductIdAndSenderIdAndReceiverId(Long productId, Long senderId, Long receiverId);
-
-//    //채팅방 별 최근 채팅 메시지 및 시간 업데이트
-//    @Query("update Chatroom c set c.latestContent = :content, c.latestContentTime = :time where c.id= :chatroomId")
-//    void updateLatest(@Param("chatroomId") Long chatroomId, @Param("content") String content, @Param("time") LocalDateTime time);
-
+    @Query("""
+            select c from Chatroom c
+            where c.productId = :productId
+              and(
+              c.senderId= :senderId and c.receiverId = :receiverId)
+              or(c.senderId= :receiverId and c.receiverId =:senderId)
+            """)
+    Optional<Chatroom> findByProductIdAndMembers(Long productId, Long senderId, Long receiverId);
 
     // 내가 sender거나 receiver인 채팅방을 최신 대화시간 내림차순으로
     List<Chatroom> findBySenderIdOrReceiverIdOrderByLatestContentTimeDesc(Long senderId, Long receiverId);
