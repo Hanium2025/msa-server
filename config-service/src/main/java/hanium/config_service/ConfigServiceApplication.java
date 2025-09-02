dc397c508701daa6b31b43f653b28e14819389e2
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-09-02 17:08:45 +0900

    [infa/#74] : config - 빈 스캔 범위 수정

--- config-service/src/main/java/hanium/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/hanium/config_service/ConfigServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "hanium.config_service")
 @EnableConfigServer
 public class ConfigServiceApplication {
     public static void main(String[] args) {
