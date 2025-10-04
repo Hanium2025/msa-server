0e9eb899145b7568e7b40584f4f3f4107430a3e5
me_nyong <gjalsdud1030@naver.com>
2025-10-04 18:16:21 +0900

    [FEAT/#109] product - 구매자만 채팅방을 만들 수 있기에 OR 조회 지우고 findByProductIdAndSenderIdAndReceiverId로 수정

--- product-service/src/main/java/hanium/product_service/repository/ChatroomRepository.java
+++ product-service/src/main/java/hanium/product_service/repository/ChatroomRepository.java
@@ -16,16 +16,7 @@ import java.util.Optional;
 public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
 
     //해당 채팅방 존재 여부 확인
-    @Query("""
-  select c from Chatroom c
-  where c.productId = :productId
-    and (
-      (c.senderId = :senderId and c.receiverId = :receiverId)
-      or
-      (c.senderId = :receiverId and c.receiverId = :senderId)
-    )
-""")
-    Optional<Chatroom> findByProductIdAndMembers(Long productId, Long senderId, Long receiverId);
+    Optional<Chatroom> findByProductIdAndSenderIdAndReceiverId(Long productId, Long senderId, Long receiverId);
 
     // 내가 sender거나 receiver인 채팅방을 최신 대화시간 내림차순으로
     List<Chatroom> findBySenderIdOrReceiverIdOrderByLatestContentTimeDesc(Long senderId, Long receiverId);
